bb0ea3d1bda64b0f3c65c866cd2bfbfa4596e751
Matt Galloway <matt@galloway.me.uk>
2020-06-12 18:28:25 +0100

    Remove SampleData and have that just in the tests themselves

--- Tests/CGTCalcCoreTests/CalculatorTests.swift
+++ Tests/CGTCalcCoreTests/CalculatorTests.swift
@@ -12,32 +12,65 @@ class CalculatorTests: XCTestCase {
 
   let logger = StubLogger()
 
-  func testSamples() throws {
-    SampleData.samples.forEach { sample in
-      do {
-        let input = CalculatorInput(transactions: sample.transactions, assetEvents: sample.assetEvents)
-        let calculator = try Calculator(input: input, logger: self.logger)
+  private struct TestData {
+    let transactions: [Transaction]
+    let assetEvents: [AssetEvent]
+    let gains: [TaxYear:Decimal]
+    let shouldThrow: Bool
+  }
 
-        let result: CalculatorResult
-        if sample.shouldThrow {
-          XCTAssertThrowsError(try calculator.process())
-          return
-        } else {
-          result = try calculator.process()
-        }
+  private func runTest(withData data: TestData) {
+    do {
+      let input = CalculatorInput(transactions: data.transactions, assetEvents: data.assetEvents)
+      let calculator = try Calculator(input: input, logger: self.logger)
 
-        XCTAssertEqual(result.taxYearSummaries.count, sample.gains.count)
-        result.taxYearSummaries.forEach { taxYearSummary in
-          guard let gain = sample.gains[taxYearSummary.taxYear] else {
-            XCTFail("Unexpected tax year found")
-            return
-          }
-          XCTAssertEqual(gain, taxYearSummary.gain)
-        }
-      } catch {
-        XCTFail("Failed to calculate \(sample.name): \(error)")
+      let result: CalculatorResult
+      if data.shouldThrow {
+        XCTAssertThrowsError(try calculator.process())
+        return
+      } else {
+        result = try calculator.process()
       }
+
+      XCTAssertEqual(result.taxYearSummaries.count, data.gains.count)
+      result.taxYearSummaries.forEach { taxYearSummary in
+        guard let gain = data.gains[taxYearSummary.taxYear] else {
+          XCTFail("Unexpected tax year found")
+          return
+        }
+        XCTAssertEqual(gain, taxYearSummary.gain)
+      }
+    } catch {
+      XCTFail("Failed to calculate")
     }
+  }
+
+  func testBasicSingleAsset() throws {
+    let testData = TestData(
+      transactions: [
+        ModelCreation.transaction(1, .Sell, "28/11/2019", "Foo", "2234.0432", "4.6702", "12.5"),
+        ModelCreation.transaction(2, .Buy, "28/08/2018", "Foo", "812.9", "4.1565", "12.5"),
+        ModelCreation.transaction(3, .Buy, "01/03/2018", "Foo", "1421.1432", "3.6093", "2"),
+      ],
+      assetEvents: [],
+      gains: [
+        TaxYear(year: 2020): Decimal(string: "1898")!,
+      ],
+      shouldThrow: false
+    )
+    self.runTest(withData: testData)
+  }
+
+  func testAssetEventWithNoAcquisition() throws {
+    let testData = TestData(
+      transactions: [],
+      assetEvents: [
+        ModelCreation.assetEvent(1, .Dividend(Decimal(1), Decimal(1)), "01/01/2020", "Foo")
+      ],
+      gains: [:],
+      shouldThrow: true
+    )
+    self.runTest(withData: testData)
   }
 
   func testDateBefore20080406Throws() throws {
