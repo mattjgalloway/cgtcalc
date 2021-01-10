31c72c0ad4ae5e84b44491d6985784094d0dd526
Matt Galloway <matt@galloway.me.uk>
2021-01-10 14:11:52 +0000

    Add another test for matching processor to cover case with asset events

--- Tests/CGTCalcCoreTests/MatchingProcessorTests.swift
+++ Tests/CGTCalcCoreTests/MatchingProcessorTests.swift
@@ -52,4 +52,43 @@ class MatchingProcessorTests: XCTestCase {
     // Processed 3 disposals so 3 matches
     XCTAssertEqual(state.disposalMatches.count, 3)
   }
+
+  func testWithAssetEvents() throws {
+    let acquisition1 = ModelCreation.transaction(.Buy, "01/01/2020", "Foo", "1000", "2", "10")
+    let acquisition2 = ModelCreation.transaction(.Buy, "20/01/2020", "Foo", "2000", "1.7", "15")
+    let acquisition1Sub = TransactionToMatch(transaction: acquisition1)
+    let acquisition2Sub = TransactionToMatch(transaction: acquisition2)
+
+    let disposal1 = ModelCreation.transaction(.Sell, "10/01/2020", "Foo", "1000", "2.5", "2")
+    let disposal1Sub = TransactionToMatch(transaction: disposal1)
+
+    let state = AssetProcessorState(
+      asset: "Foo",
+      acquisitions: [acquisition1Sub, acquisition2Sub],
+      disposals: [disposal1Sub],
+      assetEvents: [
+        ModelCreation.assetEvent(.CapitalReturn(1000, 0.1), "02/01/2020", "Foo"),
+        ModelCreation.assetEvent(.Dividend(1000, 0.4), "02/01/2020", "Foo"),
+        ModelCreation.assetEvent(.Split(4), "15/01/2020", "Foo"),
+        ModelCreation.assetEvent(.Unsplit(2), "16/01/2020", "Foo")
+      ])
+
+    let sut = MatchingProcessor(state: state, logger: self.logger)
+    try sut.process()
+
+    XCTAssertEqual(state.matchedAcquisitions.count, 1)
+    XCTAssertEqual(state.processedDisposals.count, 1)
+    XCTAssertEqual(state.pendingAcquisitions.count, 1)
+    XCTAssertEqual(state.pendingDisposals.count, 0)
+    XCTAssertEqual(state.disposalMatches.count, 1)
+
+    if let disposalMatch1 = state.disposalMatches.first {
+      // Should match the disposal against the acquisition 10 days later under B&B rules
+      let isBandB = { () -> Bool in
+        if case DisposalMatch.Kind.BedAndBreakfast = disposalMatch1.kind { return true } else { return false }
+      }()
+      XCTAssertTrue(isBandB)
+      XCTAssertEqual(disposalMatch1.gain, -917)
+    }
+  }
 }
